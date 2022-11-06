360c3f77582935074320cc51a1521ad2c45a46f3
ikkrit <johan.car@protonmail.com>
2022-11-06 23:37:31 +0100

    nav

--- Views/home/home_index.html.php
+++ Views/home/home_index.html.php
@@ -1,3 +1,3 @@
 <div class="container">
-
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet quaerat repellat, repudiandae ad velit sed quod qui natus dignissimos sunt iste harum commodi eum deserunt aspernatur ipsum ex cumque alias!</p>
 </div>
