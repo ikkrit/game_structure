551a4b8c4b9ff7368177df70cf4140fc3a4ba856
ikkrit <johan.car@protonmail.com>
2022-11-12 23:42:24 +0100

    m

--- Views/characters/characters_index.html.php
+++ Views/characters/characters_index.html.php
@@ -1,7 +1,13 @@
-<h1>Les Personnages</h1>
-<?php foreach($characters as $character): ?>
-    <article>
-        <h2><?=utf8_encode($character->character_class);?></h2>
-        <a href="characters/profil/<?=$character->character_id;?>">En savoir plus</a>
-    </article>
-<?php endforeach; ?>
+<section class="character__container">
+    <h1>Les Personnages</h1>
+
+    <div class="characters">
+        <?php foreach($characters as $character): ?>
+            <article>
+                <h2><?=utf8_encode($character->character_class);?></h2>
+                <a href="characters/profil/<?=$character->character_id;?>">En savoir plus</a>
+            </article>
+        <?php endforeach; ?>
+    </div>
+
+</section>
