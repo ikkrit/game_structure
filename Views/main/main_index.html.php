fbfd6fe2f24a70231969b2bbe8a10618d6ae3314
ikkrit <johan.car@protonmail.com>
2022-11-13 21:54:29 +0100

    main

--- Views/main/main_index.html.php
+++ Views/main/main_index.html.php
@@ -1,2 +1,21 @@
-<p>Page d'accueil du site</p>
-<h2><a href="/home">Home</a></h2>
+<section class="showcase">
+
+    <header class="showcase__header">
+
+        <h2 class="showcase__logo">rpg nav</h2>
+
+    </header>
+
+    <video src="/assets/video/main_back.mp4" muted loop autoplay></video>
+
+    <div class="showcase__overlay"></div>
+
+    <div class="showcase__text">
+        <h2>Never Stop</h2>
+        <h3>Exploring The World</h3>
+        <p>Page d'accueil du site</p>
+        <a href="/home">Decouvrir</a>
+    </div>
+
+</section>
+
