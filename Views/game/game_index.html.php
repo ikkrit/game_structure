6286b645594dd29804a98793e8be5f45d133a5a6
ikkrit <johan.car@protonmail.com>
2022-11-14 13:05:56 +0100

    game

--- Views/game/game_index.html.php
+++ Views/game/game_index.html.php
@@ -1,2 +1,12 @@
-<h1>Le jeux</h1>
+<section class="game__container">
+    <h1>Le jeux</h1>
+
+    <div class="game">
+        <div class="game__head">
+            <h2>Rpg nav </h2>
+            <p>Le jeu de rôle Dungeons & Dragons consiste à narrer des aventures dans des mondes peuplés de guerriers et de magiciens. Tout comme les jeux où l'on s'amuse à faire semblant, D&D repose sur l'imagination.Dans ce monde fantastique, les possibilités sont infinies.Prêt à en savoir plus sur le jeu de rôle D&D ? Commençons par introduire quelques concepts clés !</p>
+        </div>
+    </div>
+
+</section>
 
--- Views/main/main_index.html.php
+++ Views/main/main_index.html.php
@@ -11,9 +11,9 @@
     <div class="showcase__overlay"></div>
 
     <div class="showcase__text">
-        <h2>Never Stop</h2>
-        <h3>Exploring The World</h3>
-        <p>Page d'accueil du site</p>
+        <h2>nav rpg</h2>
+        <h3>le rpg sur navigateur</h3>
+        <p>L'histoire dont tu est le hero</p>
         <a href="/home">Decouvrir</a>
     </div>
 
