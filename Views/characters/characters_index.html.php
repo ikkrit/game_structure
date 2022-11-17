8d5a965466389b1c1bfb14b73c329da5760203b4
ikkrit <johan.car@protonmail.com>
2022-11-17 23:18:05 +0100

    life

--- Views/characters/characters_index.html.php
+++ Views/characters/characters_index.html.php
@@ -1,5 +1,5 @@
 <section class="characters__container">
-    <h1>Les Personnages</h1>
+    <h1 class="characters__title">Les Personnages</h1>
 
     <div class="characters">
 
@@ -7,7 +7,6 @@
             <div class="characters__card">
                 <div class="characters__content">
                     <h2><?=utf8_encode($character->character_name);?></h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero temporibus nulla veritatis autem, voluptates quaerat natus saepe a </p>
                     <a href="characters/profil/<?=$character->character_id;?>">En savoir plus</a>
                 </div>
                 <img src="/assets/img/cards/card<?=$character->character_id;?>.png" alt="image ">
--- Views/characters/characters_profil.html.php
+++ Views/characters/characters_profil.html.php
@@ -1,12 +1,68 @@
-<h2>Le profil du personnage</h2>
-<br>
-<br><br>
-<article>
-    <h1><?=$characters->character_name;?></h1>
-    <p><?=$characters->character_life;?></p>
-    <p><?=utf8_encode($characters->character_weapon);?></p>
-    <p><?=utf8_encode($characters->character_element);?></p>
-    <p><?=utf8_encode($characters->character_description);?></p>
-    <img src=<?=$characters->character_img;?> alt="">
-    <img src=<?=$characters->character_back;?> alt="">
-</article>
+<?php
+    function element($element)
+    {
+        switch($element) {
+            case 1:
+                echo "Lumiere";
+                break;
+            case 2;
+                echo "Eau";
+                break;
+            case 3:
+                echo "Feu";
+                break;
+            case 4: 
+                echo "Ténèbres";
+                break;
+            default:
+                echo "Aucun";
+        }
+    }
+?>
+
+<div class="profils__container">
+    <article>
+
+        <h2 class="characters__title"><?=$characters->character_name;?></h2>
+
+        <img src=<?=$characters->character_img;?> alt="<?=$characters->character_name;?>">
+        
+        <div class="profil__life">
+            <div class="life__img">
+                <img src="/assets/img/logos/heart.png" alt="">
+            </div>
+            <div class="life__text">
+                <p><?=$characters->character_life;?></p>
+            </div>
+        </div>
+
+        <div class="profil__weapon">
+            <div class="weapon__img">
+                <img src="/assets/img/logos/weapon.png" alt="arme <?=utf8_encode($characters->character_weapon);?>">
+            </div>
+            <div class="weapon__text">
+                <h3>Arme :<?=utf8_encode($characters->character_weapon);?></h3>
+            </div>
+        </div>
+
+        <div class="profil__element">
+            <div class="element__img">
+                <img src="/assets/img/logos/<?=$characters->character_element;?>" alt="">
+            </div>
+            <div class="element_text">
+                <h4>Element :<?php element($characters->character_element)?></h4>
+            </div>
+        </div>
+
+        <div class="profil__desc">
+            <div class="desc__text">
+                <p><?=utf8_encode($characters->character_description);?></p>
+            </div>
+
+            <div class="desc__img">
+                <img src=<?=$characters->character_back;?> alt="<?=$characters->character_name;?>">
+            </div>
+        </div>
+    
+    </article>
+</div>
