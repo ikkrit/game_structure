3d856f634138de64270ad9546e51cd520fa16195
ikkrit <johan.car@protonmail.com>
2022-12-11 18:08:34 +0100

    form

--- Views/game/game_start.html.php
+++ Views/game/game_start.html.php
@@ -13,10 +13,16 @@
 
         <div class="start__intro">
 
-            <div class="start__intro-text">
+            <div class="intro__text">
                 <h2>Chapitre 1 : <span><?=utf8_encode($locations->location_name);?></span>(<?=$locations->location_time;?> après JC)</h2>
                 <h3><?=ConstantsGameChapOne::CHAP_01_TITLE_01;?></h3>
                 <p><?=ConstantsGameChapOne::CHAP_01_INTRO_PRESENT_01;?></p>
+            </div>
+
+            <div class="intro__img">
+                <img src="/assets/img/game/chap_one/chap_one_1.webp" alt="">
+                <img src="/assets/img/game/chap_one/chap_one_2.webp" alt="">
+                <img src="/assets/img/game/chap_one/chap_one_3.webp" alt="">
             </div>
 
         </div>
